6903993d448acc5b4fa8e212e2bf82bb9f4c857c
sirexeclp <sirexeclp@gmail.com>
2019-02-06 20:46:43 +0100

    write_delay,  read and windows stuff

--- connect_to_erika.py
+++ connect_to_erika.py
@@ -2,25 +2,37 @@ import serial
 import time
 # replace with the USB port that you are connecting on 
 
-device = '/dev/ttyACM0'
+device = 'COM3'
 
-connection = serial.Serial(device, 1200, timeout=0, parity=serial.PARITY_EVEN, rtscts=1)
+connection = serial.Serial(device, 1200)#, timeout=0, parity=serial.PARITY_EVEN, rtscts=1)
+
+def write_delay(data, delay=0.5):
+	connection.write(data.encode("ASCII"))
+	time.sleep(delay)
 
 def print_smiley(): 
-	connection.write(b'\x13')
-	time.sleep(0.2)
-	connection.write(b'\x1F')
-	time.sleep(0.2)
+	write_delay('\x13')
+	write_delay('\x1F')
 
 def advance_paper(): 
 	# move paper up / cursor down
 	for i in range (0, 10):
 	    connection.write(b'\x75')
-	    time.sleep(0.2)
+	    time.sleep(0.5)
 
 def demo(): 
 	advance_paper()
 	print_smiley()
 	advance_paper()
 
-demo()
+#TODO: use time parameter instead of fixed value
+def alarm(time):
+	connection.write(b"\xaa\xff")
+
+def read():
+	pass
+
+#demo()
+
+while True:
+ 	print(connection.read())
