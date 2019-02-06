05473c3928306c5101490937c4764b8d801acb69
sirexeclp <sirexeclp@gmail.com>
2019-02-06 23:42:54 +0100

    objectification

--- connect_to_erika.py
+++ connect_to_erika.py
@@ -1,46 +1,64 @@
 import serial
 import time
 from string import ascii_lowercase
+import json
+
 # replace with the USB port that you are connecting on 
 
-device = 'COM3'
+class Erika:
 
-connection = serial.Serial(device, 1200)#, timeout=0, parity=serial.PARITY_EVEN, rtscts=1)
+	conversion_table_path = "charTranslation.json"
+	
+	def __init__(self, com_port, *args, **kwargs):
+		self.com_port = com_port
+		self.connection = serial.Serial(com_port, 1200)#, timeout=0, parity=serial.PARITY_EVEN, rtscts=1)
+		self.readConversionTable()
+		self.ascii_2_ddr = {}
+		self.ddr_2_ascii = {}
+	
+	def readConversionTable(self):
+		"""read conversion table from file and populate 2 dicts"""	
+		with open(self.conversion_table_path) as f:
+			self.ascii_2_ddr = json.load(f)
+		self.ddr_2_ascii = {value:key for key,value in ascii_2_ddr}
 
-def write_delay(data, delay=0.5):
-	write_byte_delay(data.encode("ASCII"),delay)
+	def write_delay(self,data, delay=0.5):
+		self.write_byte_delay(data.encode("ASCII"),delay)
 
-def write_byte_delay(data, delay = 0.5):
-	connection.write(data)
-	time.sleep(delay)
+	def write_byte_delay(self,data, delay = 0.5):
+		self.connection.write(data)
+		time.sleep(delay)
 
-def print_smiley(): 
-	write_delay('\x13')
-	write_delay('\x1F')
+	def print_smiley(self): 
+		self.write_delay('\x13')
+		self.write_delay('\x1F')
 
-def advance_paper(): 
-	# move paper up / cursor down
-	for i in range (0, 10):
-	    connection.write(b'\x75')
-	    time.sleep(0.5)
+	def advance_paper(self): 
+		# move paper up / cursor down
+		for i in range (0, 10):
+			self.connection.write(b'\x75')
+			time.sleep(0.5)
 
-def demo(): 
-	advance_paper()
-	print_smiley()
-	advance_paper()
+	def demo(self): 
+		self.advance_paper()
+		self.print_smiley()
+		self.advance_paper()
 
-#TODO: use time parameter instead of fixed value
-def alarm(time):
-	connection.write(b"\xaa\xff")
+	#TODO: use time parameter instead of fixed value
+	def alarm(self,time):
+		self.connection.write(b"\xaa\xff")
 
-def read():
-	pass
+	def read():
+		pass
 
-def print_ascii(text):
-	for c in text:
-		key_id = dict[c]
-		write_byte_delay(key_id)
+	def print_ascii(self,text):
+		for c in text:
+			key_id = dict[c]
+			self.write_byte_delay(key_id)
 
+meine_erika = Erika("COM3")
+meine_erika.demo()
+meine_erika.alarm()
 # demo()
 # alarm(None)
 
@@ -50,18 +68,5 @@ def print_ascii(text):
 # 	print(char)
 
 
-import pickle
 
-# with open("charTranslation.pkl","wb") as outf:
-# 	pickle.dump(dict, outf)
-dict = {}
-with open("charTranslation.pkl","rb") as inf:
-	dict = pickle.load(inf)
 
-# while True:
-# 	tmp = connection.read()
-# 	real_char = [key for key,value in dict.items() if value == tmp ]
-# 	print(real_char)
-# connection.close()
-
-print_ascii("halloweltaufderschreibmaschine")
